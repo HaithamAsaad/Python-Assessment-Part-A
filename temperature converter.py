d1091f87cfd7b3e0db6734a8865514317d5f12db
Haitham <41482637+HaithamAsaad@users.noreply.github.com>
2018-08-23 22:38:58 -0400

    new

--- temperature converter.py
+++ temperature converter.py
@@ -4,31 +4,36 @@ Created on Mon Jul 23 13:48:31 2018
 
 @author: haithama
 """
-Fflg=0; Cflg=0;
 x = int(input("input temerature mode 1 for C to F and 2 for F to C "))
-T = int(input("input temerature "))
-if x==1:
-    Fflg=1;
-    F = ((T + 40)* 9/5) - 40
-    print("temperature in F is ",F)
-elif x==2:
-    Cflg=1;
-    C = ((T + 40)* 5/9) - 40
-    print("temperature in C is ",C)
-    
-else:print("wrong temperature mode selection, please re enter your choice")
-
-
-if ((F<=32 and Fflg==1) or (C<=0 and Cflg==1)):
-    print("very cold")
-elif ((32<F<=50 and Fflg==1) or (0<C<=20 and Cflg==1)):
-    print("cold")
-elif ((50<F<=75 and Fflg==1) or (20<C<=38 and Cflg==1)):
-    print("worm")
-elif ((75<F and Fflg==1) or (38<C and Cflg==1)):
-    print("hot")
-    
-    
-    
-      
-    
+if x == 1:
+    T = int(input("input celsius temerature "))
+    if (T > -20 and T < 70):
+        F = round((T + 40) * 9/5) - 40
+        print("temperature in F is ", F)
+        if F <= 32:
+            print("very cold")
+        elif (F > 32 and F <= 50):
+            print("cold")
+        elif (F > 50 and F <= 75):
+            print("worm")
+        elif F > 75:
+            print("hot")
+    else:
+        print("temperature is out of range")
+elif x == 2:
+    T = int(input("input fahrenheit temerature "))
+    if (T > -5 and T < 120):
+        C = round((T + 40) * 5/9) - 40
+        print("temperature in C is ", C)
+        if C <= 0:
+            print("very cold")
+        elif (C > 0 and C <= 20):
+            print("cold")
+        elif (C > 20 and C <= 38):
+            print("worm")
+        elif C > 38:
+            print("hot")
+    else:
+        print("temperature is out of range, try again")
+else:
+    print("wrong temperature mode selection, please re-enter your choice")
